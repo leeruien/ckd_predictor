80600c6533bacddc1c1d7dbb865db783dde7d810
leeruien <ljo312342@gmail.com>
2025-03-01 20:54:51 +0800

    Updated file1 and file2 with new changes

--- clean.py
+++ clean.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('kidney_disease.csv')
+# Misclassified data
 data[['pcv', 'wc', 'rc']] = data[['pcv', 'wc', 'rc']].apply(pd.to_numeric, errors='coerce')
 data_cleaned = data
 numeric_columns = ['age', 'bp','bgr','bu','sc','sod','pot','hemo','pcv','wc','rc']
@@ -11,6 +12,7 @@ data_cleaned[numeric_columns] = data[numeric_columns].fillna(data[numeric_column
 non_numeric_columns = ['sg', 'al','su','rbc','pc','pcc','ba','htn','dm','cad','appet','pe','ane'] 
 for column in non_numeric_columns:
     data_cleaned[column] = data[column].fillna(data[column].mode()[0])
-
+# one Nan value in target variable
+data.replace('ckd\t', 'ckd', inplace = True)
 data = data.drop(['id'], axis = 1)
 print(data_cleaned.isnull().sum())
